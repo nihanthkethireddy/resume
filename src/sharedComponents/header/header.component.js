c40a9f3d1e6ad14dc60ac89a9bc55e7c38403dc3
Nikitha Kethireddy <kethireddynikitha@gmail.com>
2022-02-27 19:07:04 -0600

    Fix fonts and Nav Bar

--- src/sharedComponents/header/header.component.js
+++ src/sharedComponents/header/header.component.js
@@ -6,12 +6,17 @@ function Header() {
 
     const [renderMobileNav, setRenderMobileNav] = useState(false);
     const [activeLink, setActiveLink] = useState(window.location.hash && window.location.hash.split('#')[1] || 'home')
+
+    const setActiveMobileNav = (link) => {
+        setRenderMobileNav(false)
+        setActiveLink(link)
+    }
     
     return <div {...navStyles} className={renderMobileNav ? 'responsive' : ''}>
-        <a href={'#home'} className={activeLink == 'home' ? 'active' : ''}>Home</a>
-        <a href={'#skills'} className={activeLink == 'skills' ? 'active' : ''}>Skills</a>
-        <a href={'#projects'} className={activeLink == 'projects' ? 'active' : ''}>Projects</a>
-        <a href={'#contact'} className={activeLink == 'contact' ? 'active' : ''}>Contact</a>
+        <a href={'#home'} onClick={() => setActiveMobileNav('home')} className={activeLink == 'home' ? 'active' : ''}>Home</a>
+        <a href={'#skills'} onClick={() => setActiveMobileNav('skills')} className={activeLink == 'skills' ? 'active' : ''}>Skills</a>
+        <a href={'#projects'} onClick={() => setActiveMobileNav('projects')} className={activeLink == 'projects' ? 'active' : ''}>Projects</a>
+        <a href={'#contact'} onClick={() => setActiveMobileNav('contact')} className={activeLink == 'contact' ? 'active' : ''}>Contact</a>
         <a className="icon" onClick={() => setRenderMobileNav(!renderMobileNav)}>
             <i className="fa fa-bars"></i>
         </a>
