dde1193949a0f1ba0cc5b6e8d6b162d5bd4197cd
Nikitha Kethireddy <kethireddynikitha@gmail.com>
2022-03-30 21:29:52 -0500

    Updated Summary

--- src/sharedComponents/hero/hero.component.js
+++ src/sharedComponents/hero/hero.component.js
@@ -9,7 +9,7 @@ class Hero extends Component {
                 <div>
                     <i className={"fa fa-quote-left"}></i>
                     <div>
-                        In my past experience, I’ve developed multiple Web Based and Mobile based good reputed projects. Projects like Cars.com, ShopStevie.com, Pickuptrucks.com, Auto.com and other Products like Brutus (a traffic model simulation) etc. I am progressive minded and in tune with new development in my field. I have proven to be effective in terms of communication and collaborative with strong problem-solving talents which let me achieve Employee of the Year for two consecutive years in my previous company.
+                        In my past experience, I’ve developed multiple Web Based and Mobile based good reputed projects. Projects like Cars.com, ShopStevie.com, Pickuptrucks.com, Auto.com and other Products like Brutus (a traffic model simulation) etc. I am progressive minded and in tune with new development in my field. I have proven to be effective in terms of communication and collaborative with strong problem-solving talents.
                     </div>
                     <i className={"fa fa-quote-right"}></i>
                 </div>
